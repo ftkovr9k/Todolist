ed7a002d5513f5350ac84bd3752fa68e14e0cca9
Farhan Tanvir Khan <90358727+ftkovr9k@users.noreply.github.com>
2023-02-06 20:11:27 -0500

    Add env and engine

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try{
-    const conn = await mongoose.connect("mongodb+srv://ftkovr9k:RagingDemon667@cluster0.znsg1if.mongodb.net/todolistDB");
+    // const conn = await mongoose.connect("mongodb+srv://ftkovr9k:RagingDemon667@cluster0.znsg1if.mongodb.net/todolistDB");
+    mongoose.connect("mongodb+srv://"+process.env.ADMIN_NAME +":"+process.env.ADMIN_PASS+"@cluster0.znsg1if.mongodb.net/todolistDB", {useNewUrlParser: true});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
